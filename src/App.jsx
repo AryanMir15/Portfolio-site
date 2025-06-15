a79d23e6e0e73666fff6ebba5646b9c34275fa45
AryanMir15 <aryanmir115@gmail.com>
2025-06-15 08:35:18 +0500

    initial commit (will start building using shadcn right away!)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center font-semibold text-xs sm:text-3xl md:text-3xl lg:text-5xl ">
+      Let's get some Shadcn Magic in here !!
+      <button className=""> Click Me! </button>
+    </div>
+  );
+}
+
+export default App;
