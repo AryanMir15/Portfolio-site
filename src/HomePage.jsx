21bfd41dca30b937d3b95770615b377eee3167ee
AryanMir15 <aryanmir115@gmail.com>
2025-06-17 17:21:52 +0500

    changed the folder name from components to sections

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
-import Faq from "./components/FAQ";
-import Contact from "./components/Contact";
+import Navbar from "./Sections/Navbar";
+import Hero from "./Sections/Hero";
+import About from "./Sections/About";
+import Skills from "./Sections/Skills";
+import Projects from "./Sections/Projects";
+import Footer from "./Sections/Footer";
+import Faq from "./Sections/FAQ";
+import Contact from "./Sections/Contact";
 
 function HomePage() {
   return (
